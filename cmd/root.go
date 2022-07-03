3fd57ef7701404a18237e2e6d40e34e8fdbf0cf0
myko <awesomeme155@gmail.com>
2022-07-03 21:44:20 +0300

    add list command and action functionality

--- cmd/root.go
+++ cmd/root.go
@@ -4,37 +4,35 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/mycok/todo_list_cli/api"
 	"github.com/mycok/todo_list_cli/colors"
 )
 
-func Execute() error {
-	fs, err := handleCmdlineFlags()
+func Execute(w io.Writer) error {
+	fs, err := handleCmdlineFlags(w)
 	if err != nil {
 		return err
 	}
 
+	// Execute commands as provided by the user.
 	switch os.Args[1] {
 	case "add":
-		c := api.Get("add")
-		// TODO: assert to api.Cmd.
-		if c == nil {
-			return nil
-		}
+		return executeCmd(w, os.Args[1], fs.Args()...)
 
-		if err = c.Run(os.Stdout, fs.Args()...); err != nil {
-			return err
-		}
+	case "list":
+		return executeCmd(w, os.Args[1], fs.Args()...)
+
 	default:
 		fmt.Fprintf(
-			os.Stdout,
+			w,
 			"%sInvalid command!%s\n",
 			colors.Red,
 			colors.Reset,
 		)
-		fmt.Fprintln(os.Stdout)
+		fmt.Fprintln(w)
 		// Show usage information
 		fs.Usage()
 	}
@@ -42,9 +40,9 @@ func Execute() error {
 	return nil
 }
 
-func handleCmdlineFlags() (*flag.FlagSet, error) {
+func handleCmdlineFlags(w io.Writer) (*flag.FlagSet, error) {
 	fs := flag.NewFlagSet("todo_list_cli", flag.ExitOnError)
-	fs.SetOutput(os.Stdout)
+	fs.SetOutput(w)
 
 	fs.Usage = func() {
 		fmt.Fprintf(
@@ -59,7 +57,7 @@ func handleCmdlineFlags() (*flag.FlagSet, error) {
 			colors.Cyan, colors.Reset,
 		)
 
-		fmt.Println()
+		fmt.Fprintln(fs.Output())
 		fmt.Fprintf(
 			fs.Output(),
 			"%sUsage information:%s\n",
@@ -69,7 +67,7 @@ func handleCmdlineFlags() (*flag.FlagSet, error) {
 
 		fs.PrintDefaults()
 
-		fmt.Println()
+		fmt.Fprintln(fs.Output())
 		fmt.Fprintf(
 			fs.Output(),
 			"%sExamples:%s\n",
@@ -77,9 +75,8 @@ func handleCmdlineFlags() (*flag.FlagSet, error) {
 			colors.Reset,
 		)
 
-		fmt.Println("-add go shopping today [Adds a single item]")
-		fmt.Println("-add [Adds multiple items using the input shell]")
-
+		fmt.Fprintln(fs.Output(), "-add go shopping today [Adds a single item]")
+		fmt.Fprintln(fs.Output(), "-add [Adds multiple items using the input shell]")
 	}
 
 	todoFileName := ".todo.json"
@@ -92,11 +89,6 @@ func handleCmdlineFlags() (*flag.FlagSet, error) {
 	fs.Bool("details", false, "List all todo tasks with details")
 	fs.Bool("completed", false, "List all todo tasks including the completed")
 
-	// Commands
-	// add := fs.Bool("add", false, "Add new todo item to the todo list")
-	// list := fs.Bool("list", false, "List all available todo items")
-	// complete := fs.Int("complete", 0, "Mark todo list item as complete")
-	// del := fs.Int("del", 0, "Delete todo list item from the list")
 	var err error
 
 	if err = fs.Parse(os.Args[2:]); err != nil {
@@ -106,7 +98,7 @@ func handleCmdlineFlags() (*flag.FlagSet, error) {
 	// Walk through all provided flags [both set and unset] and add each
 	// to the cmdFlagArgs map.
 	fs.VisitAll(func(f *flag.Flag) {
-		err = api.AddFlag(f.Name, f.Value)
+		err = api.AddFlag(f)
 		if err != nil {
 			if !errors.Is(err, api.ErrDuplicateFlag) {
 				return
@@ -119,4 +111,17 @@ func handleCmdlineFlags() (*flag.FlagSet, error) {
 	}
 
 	return fs, nil
+}
+
+func executeCmd(w io.Writer, cmd string, args ...string) error {
+	c := api.Get(cmd)
+	if c == nil {
+		return nil
+	}
+
+	if err := c.Run(w, args...); err != nil {
+		return err
+	}
+
+	return nil
 }
